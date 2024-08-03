51994c0b31b4e2f6560e3847f100cb85c737601d
Adrian Di Tosto <amditosto@gmail.com>
2024-08-03 03:19:54 -0400

    added phone number and purpose to api call

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,14 @@
 // add apis here
 import axiosInstance from "./axiosInstance";
 
-export const postData = async (text, tone) => {
+export const postData = async (text, tone, phoneNumber, purpose) => {
   try {
-    await axiosInstance.post(process.env.NEXT_PUBLIC_START_CALL_ROUTE, { text: text, tone: tone });
+    await axiosInstance.post(process.env.NEXT_PUBLIC_START_CALL_ROUTE, { 
+      text: text, 
+      tone: tone,
+      phoneNumber: phoneNumber,
+      purpose: purpose,
+    });
   } catch (error) {
     throw error;
   }
