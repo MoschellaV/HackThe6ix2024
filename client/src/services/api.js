1f750c2bfa6c3a9912f7661f630b2d3f2ff4074c
Adrian Di Tosto <amditosto@gmail.com>
2024-08-03 03:14:35 -0400

    Added tone to api call

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 // add apis here
 import axiosInstance from "./axiosInstance";
 
-export const postData = async text => {
+export const postData = async (text, tone) => {
   try {
-    await axiosInstance.post("api/generate-text", { text: text });
+    await axiosInstance.post(process.env.NEXT_PUBLIC_START_CALL_ROUTE, { text: text, tone: tone });
   } catch (error) {
     throw error;
   }
