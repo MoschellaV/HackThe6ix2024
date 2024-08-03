78887490efd80861813848f2e483f8a371057fd6
Adrian Di Tosto <amditosto@gmail.com>
2024-08-03 02:20:08 -0400

    added post request to backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,9 @@
-// add apis here
+import axiosInstance from "./axiosInstance";
+
+export const postData = async (text) => {
+    try {
+        await axiosInstance.post('api/generate-text', { text: text });
+    } catch (error) {
+        throw error
+    }
+  };
