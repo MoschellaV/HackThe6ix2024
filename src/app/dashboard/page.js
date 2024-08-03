ac4546e8bad0507456e262b10921fb25050dfac8
Adrian Di Tosto <amditosto@gmail.com>
2024-08-03 02:19:59 -0400

    added template for input field

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,37 @@
-'use client';
+'use client'
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { postData } from '@/services/api';
 import { useUserContext } from '@/context/UserContext';
 
 export default function Dashboard() {
-  const { user } = useUserContext();
+    const { user } = useUserContext();
+    const [value, setValue] = useState('');
 
-  console.log(user);
+    console.log(user);
 
-  return <>Dashboard</>;
+
+    const handleClick = () => {
+        postData(value)
+    };
+
+    return (
+        <div>
+            <TextField
+                id="filled-basic"
+                label="Filled"
+                variant="filled"
+                multiline
+                rows={4}
+                value={value} 
+                onChange={(e) => setValue(e.target.value)} 
+            />
+            <Button onClick={handleClick} variant="contained" color="primary">
+                Log Value
+            </Button>
+        </div>
+    )
 }
+
+
