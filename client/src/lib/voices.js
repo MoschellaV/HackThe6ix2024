5a690ca91124655469f1d981f82791c7b5007aba
Adrian Di Tosto <amditosto@gmail.com>
2024-08-03 20:07:00 -0400

    added img field

--- client/src/lib/voices.js
+++ client/src/lib/voices.js
@@ -1,22 +1,36 @@
+import billImg from '../assets/images/bill.jpg'
+import dorothyImg from '../assets/images/dorothy.jpg'
+import freyaImg from '../assets/images/freya.jpg'
+import willImg from '../assets/images/will.jpg'
+
+import billMP3 from '../assets/voices/bill.mp3'
+
 export const voices = {
   pqHfZKP75CvOlQylNhV4: {
     name: "Bill",
     description:
-      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill."
+      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
+    img: billImg,
+    voice: billMP3,
   },
   jsCqWAovK2LkecY7zXl4: {
     name: "Freya",
     description:
-      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill."
+      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
+      img: freyaImg,
+
   },
   bIHbv24MWmeRgasZH58o: {
     name: "Will",
     description:
-      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill."
+      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
+      img: willImg,
+
   },
   ThT5KcBeYPX3keUQqHPh: {
     name: "Dorothy",
     description:
-      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill."
+      "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
+      img: dorothyImg,
   }
 };
