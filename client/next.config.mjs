2135cddc29a32e7274c2f1c2da8538e5d1939641
Adrian Di Tosto <amditosto@gmail.com>
2024-08-03 21:33:44 -0400

    added config to mp3 files

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,27 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(mp3)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            fallback: 'file-loader',
+            publicPath: '/_next/static/sounds/',
+            outputPath: 'static/sounds/',
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
