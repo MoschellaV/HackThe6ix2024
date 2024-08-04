edecb338c8edd23d4bddc95397785fc3d81512c0
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2024-08-03 21:39:04 -0400

    fix jpg -> png

--- client/src/lib/voices.js
+++ client/src/lib/voices.js
@@ -1,39 +1,37 @@
-import billImg from '../assets/images/bill.jpg'
-import dorothyImg from '../assets/images/dorothy.jpg'
-import freyaImg from '../assets/images/freya.jpg'
-import willImg from '../assets/images/will.jpg'
-import billMP3 from '../assets/voices/bill.mp3'
-import dorothyMP3 from '../assets/voices/dorothy.mp3'
-import freyaMP3 from '../assets/voices/freya.mp3'
-import willMP3 from '../assets/voices/will.mp3'
+import billImg from "../assets/images/bill.png";
+import dorothyImg from "../assets/images/dorothy.png";
+import freyaImg from "../assets/images/freya.png";
+import willImg from "../assets/images/will.png";
+import billMP3 from "../assets/voices/bill.mp3";
+import dorothyMP3 from "../assets/voices/dorothy.mp3";
+import freyaMP3 from "../assets/voices/freya.mp3";
+import willMP3 from "../assets/voices/will.mp3";
 
 export const voices = {
   pqHfZKP75CvOlQylNhV4: {
     name: "Bill",
-    description:
-      "I love being in the wilderness, away from civilization and at one with nature.",
+    description: "I love being in the wilderness, away from civilization and at one with nature.",
     img: billImg
   },
   jsCqWAovK2LkecY7zXl4: {
     name: "Freya",
     description:
       "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
-      img: freyaImg,
-      voice: freyaMP3,
+    img: freyaImg,
+    voice: freyaMP3
   },
   bIHbv24MWmeRgasZH58o: {
     name: "Will",
     description:
       "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
-      img: willImg,
-      voice: willMP3,
-
+    img: willImg,
+    voice: willMP3
   },
   ThT5KcBeYPX3keUQqHPh: {
     name: "Dorothy",
     description:
       "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
-      img: dorothyImg,
-      voice: dorothyMP3,
+    img: dorothyImg,
+    voice: dorothyMP3
   }
 };
