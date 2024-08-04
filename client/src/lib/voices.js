22bbb11a1ca73efed99f96d7e5bad89a1bae2757
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2024-08-03 20:08:14 -0400

    remove bill voice for now

--- client/src/lib/voices.js
+++ client/src/lib/voices.js
@@ -1,36 +1,31 @@
-import billImg from '../assets/images/bill.jpg'
-import dorothyImg from '../assets/images/dorothy.jpg'
-import freyaImg from '../assets/images/freya.jpg'
-import willImg from '../assets/images/will.jpg'
-
-import billMP3 from '../assets/voices/bill.mp3'
+import billImg from "../assets/images/bill.jpg";
+import dorothyImg from "../assets/images/dorothy.jpg";
+import freyaImg from "../assets/images/freya.jpg";
+import willImg from "../assets/images/will.jpg";
 
 export const voices = {
   pqHfZKP75CvOlQylNhV4: {
     name: "Bill",
     description:
       "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
-    img: billImg,
-    voice: billMP3,
+    img: billImg
   },
   jsCqWAovK2LkecY7zXl4: {
     name: "Freya",
     description:
       "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
-      img: freyaImg,
-
+    img: freyaImg
   },
   bIHbv24MWmeRgasZH58o: {
     name: "Will",
     description:
       "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
-      img: willImg,
-
+    img: willImg
   },
   ThT5KcBeYPX3keUQqHPh: {
     name: "Dorothy",
     description:
       "Bill loves isn't afraid of doing things the hard way, and hey that's why they call him called dirty Bill.",
-      img: dorothyImg,
+    img: dorothyImg
   }
 };
