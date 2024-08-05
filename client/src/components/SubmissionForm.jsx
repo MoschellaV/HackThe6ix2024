701462d16c83274f18b2a5c61bbc00bc0ce381d1
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2024-08-05 13:42:11 -0400

    organize page,js

---
+++ client/src/components/SubmissionForm.jsx
@@ -0,0 +1,313 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import FormHelperText from "@mui/material/FormHelperText";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { postData, saveCallData } from "@/services/api";
+import { Box, IconButton, Slider, Tooltip, Typography, useMediaQuery } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+
+export default function SubmissionForm({ formRef, setIsProgressActive, setSubmissionId, setActiveDoc }) {
+  const [loading, setLoading] = useState(false);
+  const [openAdvanced, setOpenAdvanced] = useState(false);
+
+  const schema = z.object({
+    prompt: z.string().min(1, { message: "Required" }),
+    purpose: z.string().min(1, { message: "Required" }),
+    phoneNumber: z.string().min(10, { message: "Phone number must be at least 10 digits" }),
+    lengthOfCall: z.enum(["short", "medium", "long"], { errorMap: () => ({ message: "Required" }) }),
+    tone: z.enum(["Flirty", "Funny", "Mean", "Normal"], { errorMap: () => ({ message: "Required" }) }),
+    voice: z.enum(["pqHfZKP75CvOlQylNhV4", "jsCqWAovK2LkecY7zXl4", "bIHbv24MWmeRgasZH58o", "ThT5KcBeYPX3keUQqHPh"], {
+      errorMap: () => ({ message: "Required" })
+    }),
+    stability: z.number().min(0).max(1).default(0.5),
+    similarity: z.number().min(0).max(1).default(0.75)
+  });
+
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      prompt: "",
+      purpose: "",
+      phoneNumber: "",
+      lengthOfCall: "",
+      tone: "",
+      voice: "",
+      stability: 0.5,
+      similarity: 0.75
+    }
+  });
+
+  const { handleSubmit, control, reset } = form;
+
+  const onSubmit = async data => {
+    setLoading(true);
+    setActiveDoc(null);
+    setSubmissionId(null);
+    const savedData = await saveCallData(
+      data.prompt,
+      data.tone,
+      data.phoneNumber,
+      data.purpose,
+      data.voice,
+      data.lengthOfCall,
+      data.stability,
+      data.similarity
+    );
+
+    if (savedData.status === 201) {
+      const {
+        id,
+        prompt,
+        tone,
+        phoneNumber,
+        purpose,
+        voice,
+        lengthOfCall,
+        createdAt,
+        completionStatus,
+        recordingURL,
+        stability,
+        similarity
+      } = savedData.data.document;
+
+      setSubmissionId(id);
+
+      postData(id, prompt, tone, phoneNumber, purpose, voice, lengthOfCall, stability, similarity);
+      setLoading(false);
+
+      setIsProgressActive(true);
+      setTimeout(() => {
+        let element = document.getElementById("call-progress");
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+        }
+      }, 100);
+
+      reset(form);
+    } else {
+      setLoading(false);
+      console.log("Error saving data");
+    }
+  };
+
+  return (
+    <form
+      ref={formRef}
+      id="Form"
+      onSubmit={handleSubmit(onSubmit)}
+      className="p-5 rounded-md w-full h-screen flex items-center justify-center align-center h-full w-full">
+      <div className="flex flex-col w-1/2 h-screen flex items-center justify-center">
+        <div className="flex flex-col bg-transparent p-8 rounded-lg flex flex-col items-center gap-6 shadow-lg w-full ">
+          <label className="text-2xl font-semibold">Try it out and make a call 🚀</label>
+
+          <Controller
+            control={control}
+            name="purpose"
+            render={({ field, fieldState }) => (
+              <TextField
+                label="Call subject (ex. Reminder for dad)"
+                variant="outlined"
+                fullWidth
+                size="small"
+                value={field.value ?? ""}
+                onChange={field.onChange}
+                error={!!fieldState.error}
+                helperText={fieldState.error?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="prompt"
+            render={({ field, fieldState }) => (
+              <TextField
+                label="Call details (ex. Tell dad to pick up the milk)"
+                variant="outlined"
+                fullWidth
+                size="small"
+                multiline
+                rows={4}
+                value={field.value ?? ""}
+                onChange={field.onChange}
+                error={!!fieldState.error}
+                helperText={fieldState.error?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="phoneNumber"
+            render={({ field, fieldState }) => (
+              <TextField
+                label="Recipient phone number"
+                type="tel"
+                variant="outlined"
+                fullWidth
+                size="small"
+                value={field.value ?? ""}
+                onChange={field.onChange}
+                error={!!fieldState.error}
+                helperText={fieldState.error?.message}
+              />
+            )}
+          />
+
+          <div className="flex flex-row w-full gap-2">
+            <Controller
+              control={control}
+              name="lengthOfCall"
+              render={({ field, fieldState }) => (
+                <div className="flex flex-col w-full">
+                  <FormControl variant="outlined" size="small" fullWidth>
+                    <InputLabel>Call length</InputLabel>
+                    <Select {...field} label="Call length" error={!!fieldState.error}>
+                      <MenuItem value="short">Short</MenuItem>
+                      <MenuItem value="medium">Medium</MenuItem>
+                      <MenuItem value="long">Long</MenuItem>
+                    </Select>
+                    {fieldState.error && <FormHelperText error>{fieldState.error.message}</FormHelperText>}
+                  </FormControl>
+                </div>
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="tone"
+              render={({ field, fieldState }) => (
+                <div className="flex flex-col w-full">
+                  <FormControl variant="outlined" size="small" fullWidth>
+                    <InputLabel>Tone</InputLabel>
+                    <Select {...field} label="Tone" error={!!fieldState.error}>
+                      <MenuItem value="Flirty">Flirty 🥰</MenuItem>
+                      <MenuItem value="Funny">Funny 😂</MenuItem>
+                      <MenuItem value="Mean">Mean 😡</MenuItem>
+                      <MenuItem value="Normal">Normal 😐</MenuItem>
+                    </Select>
+                    {fieldState.error && <FormHelperText error>{fieldState.error.message}</FormHelperText>}
+                  </FormControl>
+                </div>
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="voice"
+              render={({ field, fieldState }) => (
+                <div className="flex flex-col w-full">
+                  <FormControl variant="outlined" size="small" fullWidth>
+                    <InputLabel>Voice</InputLabel>
+                    <Select {...field} label="Voice" error={!!fieldState.error}>
+                      <MenuItem value="pqHfZKP75CvOlQylNhV4">Bill</MenuItem>
+                      <MenuItem value="jsCqWAovK2LkecY7zXl4">Freya</MenuItem>
+                      <MenuItem value="bIHbv24MWmeRgasZH58o">Will</MenuItem>
+                      <MenuItem value="ThT5KcBeYPX3keUQqHPh">Dorothy</MenuItem>
+                    </Select>
+                    {fieldState.error && <FormHelperText error>{fieldState.error.message}</FormHelperText>}
+                  </FormControl>
+                </div>
+              )}
+            />
+          </div>
+
+          <Box sx={{ width: "100%" }}>
+            <Box
+              sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+              onClick={() => setOpenAdvanced(!openAdvanced)}>
+              <Typography variant="body2" sx={{ fontWeight: 600, fontSize: 12, opacity: 0.4, mr: 0.5 }}>
+                Advanced settings
+              </Typography>
+              {openAdvanced ? <ExpandLessIcon sx={{ opacity: 0.3 }} /> : <ExpandMoreIcon sx={{ opacity: 0.3 }} />}
+            </Box>
+          </Box>
+
+          {openAdvanced && (
+            <>
+              <Controller
+                control={control}
+                name="stability"
+                defaultValue={0.5}
+                render={({ field, fieldState }) => (
+                  <div className="flex flex-col w-full">
+                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                      <Typography sx={{ fontSize: 14, opacity: 0.6, mb: 0 }} id="stability-slider" gutterBottom>
+                        Stability
+                      </Typography>
+                      <Tooltip
+                        title="Adjusts the voice consistency. Lower values produce a more emotive and varied performance while higher values lead to a more stable and consistent voice."
+                        placement="top"
+                        arrow>
+                        <IconButton size="small">
+                          <HelpOutlineOutlinedIcon sx={{ fontSize: 18, opacity: 0.7 }} />
+                        </IconButton>
+                      </Tooltip>
+                    </Box>
+                    <Slider
+                      {...field}
+                      aria-labelledby="stability-slider"
+                      valueLabelDisplay="auto"
+                      step={0.01}
+                      min={0}
+                      max={1}
+                      error={!!fieldState.error}
+                    />
+                    {fieldState.error && <FormHelperText error>{fieldState.error.message}</FormHelperText>}
+                  </div>
+                )}
+              />
+              <Controller
+                control={control}
+                name="similarity"
+                defaultValue={0.75}
+                render={({ field, fieldState }) => (
+                  <div className="flex flex-col w-full">
+                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                      <Typography sx={{ fontSize: 14, opacity: 0.6, mb: 0 }} id="similarity-slider" gutterBottom>
+                        Similarity
+                      </Typography>
+                      <Tooltip
+                        title="Controls how closely the AI replicates the original voice. Higher values ensure the generated voice closely matches the original, which might include artifacts or background noise if present in the source. Lower values allow for more flexibility and creativity in the voice generation."
+                        placement="top"
+                        arrow>
+                        <IconButton size="small">
+                          <HelpOutlineOutlinedIcon sx={{ fontSize: 18, opacity: 0.7 }} />
+                        </IconButton>
+                      </Tooltip>
+                    </Box>
+                    <Slider
+                      {...field}
+                      aria-labelledby="similarity-slider"
+                      valueLabelDisplay="auto"
+                      step={0.01}
+                      min={0}
+                      max={1}
+                      error={!!fieldState.error}
+                    />
+                    {fieldState.error && <FormHelperText error>{fieldState.error.message}</FormHelperText>}
+                  </div>
+                )}
+              />
+            </>
+          )}
+
+          <Button sx={{ borderRadius: "8px" }} fullWidth variant="contained" type="submit" disabled={loading}>
+            Make call
+          </Button>
+        </div>
+        <div className="mt-32"></div>
+      </div>
+    </form>
+  );
+}
